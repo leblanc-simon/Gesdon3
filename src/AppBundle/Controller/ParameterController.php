333f44f3b6641f62fded802b8cf0185554dc79ae
Simon Leblanc <contact@leblanc-simon.eu>
2015-10-28 00:29:37 +0100

    Add payment type parameter + refactoring

--- src/AppBundle/Controller/ParameterController.php
+++ src/AppBundle/Controller/ParameterController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\ContributorType;
+use AppBundle\Entity\PaymentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -48,7 +49,7 @@ class ParameterController extends Controller
 
     /**
      * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/contributor_types", name="parameters_contributor_types")
+     * @Route("/contributor-types", name="parameters_contributor_types")
      */
     public function contributorTypesAction()
     {
@@ -61,62 +62,117 @@ class ParameterController extends Controller
 
     /**
      * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/contributor_types/{id}/edit", name="parameters_contributor_types_edit")
+     * @Route("/contributor-types/{id}/edit", name="parameters_contributor_types_edit")
      */
     public function contributorTypeEditAction(Request $request, ContributorType $contributor_type)
     {
-        return $this->contributorTypeEdit($request, $contributor_type);
+        return $this->typeEdit($request, $contributor_type, 'contributor_type');
     }
 
     /**
      * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/contributor_types/add", name="parameters_contributor_types_add")
+     * @Route("/contributor-types/add", name="parameters_contributor_types_add")
      */
     public function contributorTypeAddAction(Request $request)
     {
-        return $this->contributorTypeEdit($request, new ContributorType());
+        return $this->typeEdit($request, new ContributorType(), 'contributor_type');
     }
 
     /**
      * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/contributor_types/{id}/delete", name="parameters_contributor_types_delete")
+     * @Route("/contributor-types/{id}/delete", name="parameters_contributor_types_delete")
      */
     public function contributorTypeDeleteAction(Request $request, ContributorType $contributor_type)
     {
-        $form = $this->createForm('contributor_type_delete', $contributor_type);
-        $form->handleRequest($request);
+        return $this->typeRemove($request, $contributor_type, 'contributor_type');
+    }
 
-        if ($form->isValid() === true) {
-            $entity_manager = $this->getDoctrine()->getManager();
-            $entity_manager->remove($contributor_type);
-            $entity_manager->flush();
-            $this->get('session.flash_bag')->add('success', 'item.removed');
-            return $this->redirectToRoute('parameters_contributor_types');
-        }
-
-        return $this->render('parameter/contributor_type/delete.html.twig', [
-            'form' => $form->createView(),
-            'contributor_type' => $contributor_type,
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/payment-types", name="parameters_payment_types")
+     */
+    public function paymentTypesAction()
+    {
+        return $this->render('parameter/payment_type/index.html.twig', [
+            'payment_types' => $this->get('repository.payment_type')->findBy([], [
+                'name' => 'ASC',
+            ]),
         ]);
     }
 
-    private function contributorTypeEdit(Request $request, ContributorType $contributor_type)
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/payment-types/{id}/edit", name="parameters_payment_types_edit")
+     */
+    public function paymentTypeEditAction(Request $request, PaymentType $payment_type)
     {
-        $form = $this->createForm('contributor_type', $contributor_type);
+        return $this->typeEdit($request, $payment_type, 'payment_type');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/payment-types/add", name="parameters_payment_types_add")
+     */
+    public function paymentTypeAddAction(Request $request)
+    {
+        return $this->typeEdit($request, new PaymentType(), 'payment_type');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/payment-types/{id}/delete", name="parameters_payment_types_delete")
+     */
+    public function paymentTypeDeleteAction(Request $request, PaymentType $payment_type)
+    {
+        return $this->typeRemove($request, $payment_type, 'payment_type');
+    }
+
+    /**
+     * @param Request $request
+     * @param         $object
+     * @param         $type
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    private function typeEdit(Request $request, $object, $type)
+    {
+        $form = $this->createForm($type, $object);
         $form->handleRequest($request);
 
         if ($form->isValid() === true) {
             $entity_manger = $this->getDoctrine()->getManager();
-            $entity_manger->persist($contributor_type);
+            $entity_manger->persist($object);
             $entity_manger->flush();
             $this->get('session.flash_bag')->add('success', 'item.saved');
-            return $this->redirectToRoute('parameters_contributor_types');
+            return $this->redirectToRoute('parameters_'.$type.'s');
         }
 
-        return $this->render('parameter/contributor_type/edit.html.twig', [
+        return $this->render('parameter/'.$type.'/edit.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
+    /**
+     * @param Request $request
+     * @param         $object
+     * @param         $type
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    private function typeRemove(Request $request, $object, $type)
+    {
+        $form = $this->createForm($type.'_delete', $object);
+        $form->handleRequest($request);
 
+        if ($form->isValid() === true) {
+            $entity_manager = $this->getDoctrine()->getManager();
+            $entity_manager->remove($object);
+            $entity_manager->flush();
+            $this->get('session.flash_bag')->add('success', 'item.removed');
+            return $this->redirectToRoute('parameters_'.$type.'s');
+        }
+
+        return $this->render('parameter/'.$type.'/delete.html.twig', [
+            'form' => $form->createView(),
+            $type => $object,
+        ]);
+    }
 }
